02477dab94458b4cf3915a3690ae6b9826bd845f
lavanya9100 <lavanyanaveen55@gmail.com>
2018-09-21 18:45:17 +0530

    "naveen commit"

---
+++ practice555/src/practice555/Practice555.java
@@ -0,0 +1,15 @@
+package practice555;
+
+public class Practice555 {
+
+	public static void main(String[] args) {
+		int i=10;
+		if(i%2==0){
+			System.out.println("i is even " +i);
+			
+		}else
+			System.out.println("i is odd " +i);
+	}
+	
+
+}
